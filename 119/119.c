02b5c6a9aa78b4b6aaec38e9624c3425ed9df595
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-18 08:49:43 -0700

    119: write experimental code
    Experiment with all numbers within the range of unsigned long long.
    Will it give us more than 30 results?

--- 119/119.c
+++ 119/119.c
@@ -11,7 +11,37 @@
 #include <string.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <limits.h>
+
+#define MAX_POWER ULLONG_MAX // experimental, max 20 digits
+#define MAX_BASE 180 // experimental
+
+typedef unsigned long long num_t;
+
+static inline num_t digit_sum(num_t n);
 
 int main(int argc, const char *argv[]) {
+    size_t cnt = 0; // counter
+    for (num_t base = 2; base <= MAX_BASE; base++) {
+        num_t pwr = base;
+        while (pwr <= MAX_POWER / base) {
+            pwr *= base;
+            if (digit_sum(pwr) == base) {
+                printf("%llu\n", pwr);
+                cnt++;
+            }
+        }
+    }
+    printf("count: %zu\n", cnt);
     return 0;
+}
+
+static inline num_t
+digit_sum(num_t n) {
+    num_t sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
 }
