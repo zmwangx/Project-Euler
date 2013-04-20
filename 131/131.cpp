7d9a69c4d6b68a0a940053fa9aa9c53ef53dbe04
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-20 00:43:40 -0700

    131: initial commit
    Included prime lib.

---
+++ 131/131.cpp
@@ -0,0 +1,29 @@
+/**
+ * 131.cpp
+ * Project Euler 131
+ * Created by Zhiming Wang on 04/20/2013.
+ * --------------------------------------
+ * n^3 + p n^2 = m^3
+ * -->
+ * n^2 (n + p) = m^3
+ * -->
+ * n^2 = m1^6
+ * n + p = m2^3
+ * -->
+ * m1^3 + p = m2^3
+ * -->
+ * p = (m2 - m1)(m2^2 + m2 m1 + m1^2)
+ * -->
+ * m2 = m1 + 1
+ * p = 3 m1 (m1 + 1) + 1
+ */
+
+#include "../Library/prime.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
