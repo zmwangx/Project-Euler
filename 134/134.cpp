fd8399fc50cbd3530af10d7d036e346a4902d75a
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-22 11:26:26 -0700

    134: initial commit

---
+++ 134/134.cpp
@@ -0,0 +1,23 @@
+/**
+ * 134.cpp
+ * Project Euler 134
+ * Created by Zhiming Wang on 04/22/2013.
+ * --------------------------------------
+ * Simple congruence equation problem.
+ *
+ * The equation is
+ *     10^l x + p1 = 0 (mod p2),
+ * where l is the length of p1. So we only need to solve
+ *     10^l x = p2 - p1 (mod p2).
+ */
+
+#include "../lib/prime.h"
+#include "../lib/modulo.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
