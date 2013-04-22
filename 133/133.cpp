cb10117d2051d7e8749db6efaf9efa21899efca8
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-22 10:16:14 -0700

    133: finalize (correct)

--- 133/133.cpp
+++ 133/133.cpp
@@ -24,7 +24,36 @@
 #include <iostream>
 #include <string>
 
+#define MAX_PRIME 100000
+
 int main(int argc, const char *argv[]) {
-    
+    std::vector<int> primes;
+    generatePrimes(primes, 7, MAX_PRIME); // we know that 2, 3, 5 need not be
+                                          // considered
+    arith_t sum = 0;
+    // 2, 3, 5 are not remarkable
+    sum = sum + 2 + 3 + 5;
+    for (auto p: primes) {
+        // extract 2's and 5's from p-1
+        arith_t q = (arith_t) p - 1;
+        arith_t r = 1; // holder of 2's and 5's
+        while (q % 2 == 0) {
+            q /= 2;
+            r *= 2;
+        }
+        while (q % 5 == 0) {
+            q /= 5;
+            r *= 5;
+        }
+        arith_t remainder = pwrmod(10, r, p);
+        if (remainder == 1) {
+            // remarkable
+            std::cout << p << std::endl;
+        } else {
+            // not remarkable
+            sum += p;
+        }
+    }
+    std::cout << "sum: " << sum << std::endl;
     return 0;
 }
