8c31352402904a0414c75543e263b56b06f41b85
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-22 17:01:40 -0700

    125: finish

--- 125/125.cpp
+++ 125/125.cpp
@@ -18,7 +18,54 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
+
+#define MAX_PALIN 100000000
+#define MAX_N (int(std::sqrt(MAX_PALIN / 2)) + 1)
+
+typedef signed long long sum_t;
+
+static bool is_palin(sum_t s);
 
 int main(int argc, const char *argv[]) {
+    // compute master square sums
+    sum_t s[MAX_N + 1]; // square sums from 1
+    s[0] = 0;
+    for (int i = 1; i <= MAX_N; i++) {
+        s[i] = s[i - 1] + i * i;
+    }
+    // consider all differences
+    // record the palins in a set
+    std::set<sum_t> palins;
+    for (int i = 0; i <= MAX_N - 2; i++) {
+        for (int j = i + 2; j <= MAX_N; j++) {
+            sum_t diff = s[j] - s[i];
+            if (diff >= MAX_PALIN) {
+                continue;
+            } else if (is_palin(diff)) {
+                palins.insert(diff);
+            }
+        }
+    }
+    // compute sum
+    sum_t totalSum = 0;
+    for (auto s: palins) {
+        std::cout << s << std::endl;
+        totalSum += s;
+    }
+    std::cout << totalSum << std::endl;
     return 0;
+}
+
+static bool
+is_palin(sum_t s) {
+    std::string st = std::to_string(s);
+    auto i = st.begin();   // normal order
+    auto j = st.end() - 1; // reverse order
+    for (; i < j; i++, j--) {
+        if (*i != *j) {
+            return false;
+        }
+    }
+    return true;
 }
