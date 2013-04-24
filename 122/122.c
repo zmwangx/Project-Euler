c4ee56434dc69494479e1bf3226d52f461ed2f7a
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-24 11:21:27 -0700

    122: initial commit

---
+++ 122/122.c
@@ -0,0 +1,17 @@
+/**
+ * 122.c
+ * Project Euler 122
+ * Created by Zhiming Wang on 04/24/2013.
+ * --------------------------------------
+ * IDDFS.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
