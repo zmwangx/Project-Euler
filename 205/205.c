fb42e86b1b7fbd4563e696e64d295fef7aaf4c05
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-27 15:39:29 -0700

    205: finish counting

--- 205/205.c
+++ 205/205.c
@@ -6,15 +6,40 @@
  * Brute force counting.
  */
 
-#include <iostream>
-#include <fstream>
-#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
 
+#define MAX_SUM 36
 #define NUM_BASE4 9
 #define MAX_BASE4 4 * 4 * 4 * 4 * 4 * 4 * 4 * 4 * 4
 #define NUM_BASE6 6
 #define MAX_BASE6 6 * 6 * 6 * 6 * 6 * 6
 
 int main(int argc, const char *argv[]) {
+    size_t base4SumNum[MAX_SUM + 1] = {0};
+    size_t base6SumNum[MAX_SUM + 1] = {0};
+    
+    // base 4 numbers
+    for (int n = 0; n < MAX_BASE4; n++) {
+        size_t sum = 0;
+        for (int i = 0; i < NUM_BASE4; i++) {
+            sum += n % 4;
+            n /= 4;
+        }
+        base4SumNum[sum]++;
+    }
+    // base 6 numbers
+    for (int n = 0; n < MAX_BASE6; n++) {
+        size_t sum = 0;
+        for (int i = 0; i < NUM_BASE6; i++) {
+            sum += n % 6;
+            n /= 6;
+        }
+        base6SumNum[sum]++;
+    }
+    
     return 0;
 }
