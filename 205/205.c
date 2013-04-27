caff711cb4e1a88dfbc6bd015c80a1378fc40ef8
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-27 15:35:12 -0700

    205: initial commit

---
+++ 205/205.c
@@ -0,0 +1,20 @@
+/**
+ * 205.c
+ * Project Euler 205
+ * Created by Zhiming Wang on 04/27/2013.
+ * --------------------------------------
+ * Brute force counting.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define NUM_BASE4 9
+#define MAX_BASE4 4 * 4 * 4 * 4 * 4 * 4 * 4 * 4 * 4
+#define NUM_BASE6 6
+#define MAX_BASE6 6 * 6 * 6 * 6 * 6 * 6
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
