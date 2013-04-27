88221305b79dda504abf520576eace7e51e3520c
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-27 14:36:00 -0700

    130: finish generating prime set
    Using STL algorithm and inserter. Cool!

--- 130/130.cpp
+++ 130/130.cpp
@@ -6,10 +6,24 @@
  * NT library call.
  */
 
+#include "../lib/prime.h"
+#include "../lib/modulo.h"
+
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <set>
+#include <iterator>
+#include <algorithm>
+
+#define NUM_COMPOSITE_VALUES 25
+
+#define MAX 100000 // guessed value
 
 int main(int argc, const char *argv[]) {
+    std::vector<int> pvec;
+    generatePrimes(pvec, 7, MAX);
+    std::set<int> pset;
+    std::copy(pvec.begin(), pvec.end(), std::inserter(pset, pset.end()));
     return 0;
 }
