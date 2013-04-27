17cc240a9dd23c08dcb8e41abc04518103df5e1b
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-27 15:46:56 -0700

    205: finish coding (seems buggy)

--- 205/205.c
+++ 205/205.c
@@ -12,11 +12,12 @@
 #include <stdbool.h>
 #include <assert.h>
 
+#define ullong unsigned long long
 #define MAX_SUM 36
 #define NUM_BASE4 9
-#define MAX_BASE4 4 * 4 * 4 * 4 * 4 * 4 * 4 * 4 * 4
+#define MAX_BASE4 (4 * 4 * 4 * 4 * 4 * 4 * 4 * 4 * 4)
 #define NUM_BASE6 6
-#define MAX_BASE6 6 * 6 * 6 * 6 * 6 * 6
+#define MAX_BASE6 (6 * 6 * 6 * 6 * 6 * 6)
 
 int main(int argc, const char *argv[]) {
     size_t base4SumNum[MAX_SUM + 1] = {0};
@@ -38,8 +39,20 @@ int main(int argc, const char *argv[]) {
             sum += n % 6;
             n /= 6;
         }
-        base6SumNum[sum]++;
     }
+    printf("1\n");
+
+    // compute probability
+    ullong total = (ullong) MAX_BASE4 * (ullong) MAX_BASE6;
+    ullong base4Win = 0;
+    for (size_t base4Sum = 0; base4Sum <= MAX_SUM; base4Sum++) {
+        for (size_t base6Sum = 0; base6Sum < base4Sum; base6Sum++) {
+            base4Win +=
+                (ullong) base4SumNum[base4Sum] *
+                (ullong) base6SumNum[base6Sum];
+        }
+    }
+    printf("%.7lf\n", (double) base4Win / (double) total);
     
     return 0;
 }
