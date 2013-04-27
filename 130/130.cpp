1875a519ad62f6dfc527bf3a8b373ee6ac238dac
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-27 14:45:48 -0700

    130: finish

--- 130/130.cpp
+++ 130/130.cpp
@@ -16,14 +16,32 @@
 #include <iterator>
 #include <algorithm>
 
-#define NUM_COMPOSITE_VALUES 25
+#define NUM_VALUES_EXPECTED 25
 
+#define MIN 7
 #define MAX 100000 // guessed value
 
 int main(int argc, const char *argv[]) {
     std::vector<int> pvec;
-    generatePrimes(pvec, 7, MAX);
+    generatePrimes(pvec, MIN, MAX);
     std::set<int> pset;
     std::copy(pvec.begin(), pvec.end(), std::inserter(pset, pset.end()));
+
+    int sum = 0;
+    size_t num = 0;
+    for (int n = MIN; n < MAX; n++) {
+        if (n % 2 != 0 && n % 5 != 0 && !pset.count(n)) {
+            if (pwrmod(10, n-1, 9 * n) == 1) {
+                sum += n;
+                num++;
+                std::cout << num << ": " << n << std::endl;
+                if (num == NUM_VALUES_EXPECTED) {
+                    break;
+                }
+            }
+        }
+    }
+    std::cout << sum << std::endl;
+    
     return 0;
 }
