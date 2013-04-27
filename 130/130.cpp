cb6e72029392841e9139a0c342cef1b7eb08205b
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-27 14:23:03 -0700

    130: initial commit

---
+++ 130/130.cpp
@@ -0,0 +1,15 @@
+/**
+ * 130.cpp
+ * Project Euler 130
+ * Created by Zhiming Wang on 04/27/2013.
+ * --------------------------------------
+ * NT library call.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
