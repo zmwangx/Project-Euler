e548a147f9fa088ef07f473261ac3cca39e11618
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-27 16:06:19 -0700

    179: initial commit

---
+++ 179/179.cpp
@@ -0,0 +1,17 @@
+/**
+ * 179.cpp
+ * Project Euler 179
+ * Created by Zhiming Wang on 04/27/2013.
+ * --------------------------------------
+ * Brute force all number of divisors.
+ */
+
+#include "../lib/prime.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
