cb7f5418c48cb52accd1874f483804bbb34a668e
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-27 16:29:36 -0700

    187: initial commit

---
+++ 187/187.cpp
@@ -0,0 +1,18 @@
+/**
+ * 187.cpp
+ * Project Euler 187
+ * Created by Zhiming Wang on 04/27/2013.
+ * --------------------------------------
+ *
+ */
+
+#include "../lib/prime.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
