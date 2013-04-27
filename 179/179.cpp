feb029cbf772ba5591713ac1ed24b2b53acca66e
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-27 16:11:33 -0700

    179: finish generating primes

--- 179/179.cpp
+++ 179/179.cpp
@@ -11,7 +11,21 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <set>
+#include <iterator>
+#include <algorithm>
+
+#define MAX 10000000
+#define MAX_SQRT 3162
 
 int main(int argc, const char *argv[]) {
+    std::vector<int> pvec;
+    std::set<int> pset;
+    generatePrimes(pvec, 2, MAX);
+    std::copy(pvec.begin(), pvec.end(), std::inserter(pset, pset.end()));
+
+    std::vector<int> pvecl; // lite vector; only up to MAX_SQRT for speed
+                            // concern
+    generatePrimes(pvecl, 2, MAX_SQRT);
     return 0;
 }
