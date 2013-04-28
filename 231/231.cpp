5ef2638d5812f05930897b6ddbf29d9ce247f404
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-28 12:51:04 -0700

    231: finish coding

--- 231/231.cpp
+++ 231/231.cpp
@@ -12,6 +12,35 @@
 #include <fstream>
 #include <string>
 
+static inline int factorialPrimeIndex(int n, int p);
+
+#define N 20000000
+#define M 15000000
+
 int main(int argc, const char *argv[]) {
+    std::vector<int> primes;
+    generatePrimes(primes, 2, N);
+    long long sum = 0;
+    for (auto p: primes) {
+        int index =
+            factorialPrimeIndex(N, p)
+            - factorialPrimeIndex(M, p)
+            - factorialPrimeIndex(N - M, p);
+        if (index > 0) {
+            sum += p;
+        }
+    }
+    std::cout << sum << std::endl;
     return 0;
+}
+
+// index of prime p in the factorization of n!
+static inline int
+factorialPrimeIndex(int n, int p) {
+    int index = 0;
+    while (n > 1) {
+        index += n % p;
+        n /= p;
+    }
+    return index;
 }
