fb0672266eea1cc3b3ce1d33be541cafe1ad00ea
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-28 12:46:30 -0700

    231: initial commit

---
+++ 231/231.cpp
@@ -0,0 +1,17 @@
+/**
+ * 231.cpp
+ * Project Euler 231
+ * Created by Zhiming Wang on 04/28/2013.
+ * --------------------------------------
+ * Prime factorization of factorials.
+ */
+
+#include "../lib/prime.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
