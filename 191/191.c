7799cce302e91b07d3c75b1661f8c165989d9a3a
Zhiming Wang <ZhimingWangPublic@gmail.com>
2013-04-28 09:39:47 -0700

    191: finish coding

--- 191/191.c
+++ 191/191.c
@@ -23,6 +23,26 @@ int main(int argc, const char *argv[]) {
     ull_t f4[LENGTH + 1]; // late, 0 consec absent
     ull_t f5[LENGTH + 1]; // late, 1 consec absent
     ull_t f6[LENGTH + 1]; // late, 2 consec absent
+
+    f1[0] = 1;
+    f2[0] = 0;
+    f3[0] = 0;
+    f4[0] = 0;
+    f5[0] = 0;
+    f6[0] = 0;
+    for (size_t i = 0; i <= LENGTH; i++) {
+        f1[i] = f1[i-1] + f2[i-1] + f3[i-1];
+        f2[i] = f1[i-1];
+        f3[i] = f2[i-1];
+        f4[i] = f1[i-1] + f2[i-1] + f3[i-1] + f4[i-1] + f5[i-1] + f6[i-1];
+        f5[i] = f4[i-1];
+        f6[i] = f5[i-1];
+    }
+
+    ull_t total =
+        f1[LENGTH] + f2[LENGTH] + f3[LENGTH] +
+        f4[LENGTH] + f5[LENGTH] + f6[LENGTH];
+    printf("%llu\n", total);
     
     return 0;
 }
