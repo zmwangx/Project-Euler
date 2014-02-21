30b945ba023aaa8b937ff06d39d55f33f46e8649
Zhiming Wang <ZhimingWangPublic@gmail.com>
2014-02-21 15:29:41 -0800

    finished decide-range.c

--- 127/decide-range.c
+++ 127/decide-range.c
@@ -12,6 +12,19 @@
 #include <stdbool.h>
 #include <assert.h>
 
+#include "read-rad.h"
+
 int main(int argc, const char *argv[]) {
+    read_rad();
+    printf("enter the threshold value c/rad(c):\n");
+    long threshold;
+    scanf("%ld", &threshold);
+    long counter = 0;
+    for (int c = 1; c <= N; c++) {
+        if (_rad[c] * threshold < c) {
+            counter++;
+        }
+    }
+    printf("number of c's that passed the threshold: %ld\n", counter);
     return 0;
 }
